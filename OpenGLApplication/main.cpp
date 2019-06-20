321121f4a0ee3146bcc73e1636aca4b6c36b784a
antoniorv6 <ariosvila@gmail.com>
2019-06-20 12:21:06 +0200

    GLM and ImGUI set to the project

--- OpenGLApplication/main.cpp
+++ OpenGLApplication/main.cpp
@@ -3,7 +3,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-const GLint WIDTH = 800, HEIGHT = 600;
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include <glm/glm.hpp>
+const GLint WIDTH = 1280, HEIGHT = 720;
 
 int main()
 {
@@ -44,16 +49,38 @@ int main()
 
 	glViewport(0,0, bufferWidth, bufferHeight);
 
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(mainWindow, true);
+	ImGui_ImplOpenGL3_Init("#version 150");
+	ImGui::StyleColorsDark();
+
+	glm::vec3 newMat = glm::vec3(0,0,0);
+	std::cout << newMat.x << std::endl;
+
 	while (!glfwWindowShouldClose(mainWindow))
 	{
 		glfwPollEvents();
 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Hello world!");
+		ImGui::End();
+
+		ImGui::Render();
+		
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		glfwSwapBuffers(mainWindow);
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 	glfwDestroyWindow(mainWindow);
 	glfwTerminate();
 
